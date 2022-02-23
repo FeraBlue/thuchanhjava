4008ee72de4565c65dab4a5ced7f732fb6982be1
Hồng Quân Nguyễn <ferablue159x@gmail.com>
2022-02-23 15:09:09 +0700

    commit note

---
+++ baithuchanh2202/src/App10.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App10 {
+    public static void main(String[] args) throws Exception {
+        int n, count = 0;
+        Scanner sc = new Scanner(System.in);
+        do{
+          System.out.println("Hay nhap vao so nguyen duong : ");
+          n = sc.nextInt();
+        }while(n<=0);
+  
+        while (n > 0) {
+            n /= 10;
+            count++;
+        }
+        System.out.println("So nay co " + count + " so");
+        sc.close();
+    }
+  }
